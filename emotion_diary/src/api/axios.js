f62c0eee4055db6b0aca8de1a6b1b484a59872b7
sleepyencoder <kthkth38@naver.com>
2025-07-14 20:02:40 +0900

    real start

--- emotion_diary/src/api/axios.js
+++ emotion_diary/src/api/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // axios 인스턴스 생성
 const instance = axios.create({
-  baseURL: "http://localhost:8080", // API 서버 주소
-  timeout: 5000, // 5초 타임아웃
+  baseURL: "http://3.34.199.68:8080", // API 서버 주소
+  timeout: 1000, // 5초 타임아웃
 });
 
 export default instance;
